87264d2926054a79edb9176878263785ddcfc476
ryant <ryanlui.college@gmail.com>
2020-01-28 04:33:17 -0800

    Added test set

--- main.py
+++ main.py
@@ -8,18 +8,18 @@ import random
 import datetime
 import numpy as np
 
-ETA = 0.1
-LOGFILE = 'log.txt'
+ETA = 0.001
+LOGFILE = 'eta001.txt'
 CLASSES = 10
 
 
-def train_on_set(train_images, train_labels, weights, size, learning_rate, epochs):
+def train_on_set(train_images, train_labels, test_images, test_labels, weights, size, learning_rate, epochs):
     with open(LOGFILE, 'a') as file:
-        #file.write("%s: Beginning training.\n" % (datetime.datetime.now()))
+        file.write("%s: Beginning training.\n" % (datetime.datetime.now()))
         print("%s: Beginning training.\n" % (datetime.datetime.now()))
-        #file.write("%s: Current accuracy is %.5f.\n" % (datetime.datetime.now(), check_accuracy(train_images, train_labels, weights, size)))
-        print("%s: Current accuracy is %.5f.\n" % (
-        datetime.datetime.now(), check_accuracy(train_images, train_labels, weights, size)))
+        prev_acc = check_accuracy(train_images, train_labels, weights, size)
+        file.write("%s: Current accuracy is %.5f.\n" % (datetime.datetime.now(), prev_acc))
+        print("%s: Current accuracy is %.5f.\n" % (datetime.datetime.now(), prev_acc))
         for e in range(0, epochs):
             #file.write("%s: Beginning epoch %d.\n" % (datetime.datetime.now(), e+1))
             print("%s: Beginning epoch %d.\n" % (datetime.datetime.now(), e + 1))
@@ -41,21 +41,23 @@ def train_on_set(train_images, train_labels, weights, size, learning_rate, epoch
                             else:
                                 weights[i, j] -= learning_rate * (threshold[i] - 0) * train_images[n, j]
             #file.write("%s: Epoch %d complete.\n" % (datetime.datetime.now(), e+1))
-            #file.write("%s: Accuracy for epoch %d is %.5f.\n" % (
-            #datetime.datetime.now(), e+1, check_accuracy(train_images, train_labels, weights, size)))
+            accuracy = check_accuracy(train_images, train_labels, weights, size)
+            test_acc = check_accuracy(test_images, test_labels, weights, 10000)
+            file.write("%s:%d, %.5f, %.5f.\n" % (datetime.datetime.now(), e+1, accuracy, test_acc))
             print("%s: Epoch %d complete.\n" % (datetime.datetime.now(), e + 1))
-            print("%s: Accuracy for epoch %d is %.5f.\n" % (
-                datetime.datetime.now(), e + 1, check_accuracy(train_images, train_labels, weights, size)))
+            print("%s: Accuracy for epoch %d is %.5f.\n" % (datetime.datetime.now(), e + 1, accuracy))
+            if abs(prev_acc-accuracy)*100 < 0.01:
+                break
 
 
-def check_accuracy(train_images, train_labels, weights, size):
+def check_accuracy(images, labels, weights, size):
     perceptrons = np.zeros(10)
     correct = 0
     for n in range(0, size):
         for i in range(0, CLASSES):
-            perceptrons[i] = np.dot(train_images[n], weights[i])
+            perceptrons[i] = np.dot(images[n], weights[i])
         prediction = np.argmax(perceptrons)
-        if prediction == train_labels[n]:
+        if prediction == labels[n]:
             correct += 1
     return correct/size
 
@@ -63,12 +65,15 @@ def check_accuracy(train_images, train_labels, weights, size):
 if __name__ == '__main__':
     mndata = MNIST('./images/')
     train_images, train_labels = mndata.load_training()
-    #test_images, test_labels = mndata.load_testing()
+    test_images, test_labels = mndata.load_testing()
     np_train = np.asarray(train_images, dtype=np.float16).reshape(60000, 784) / 255  # divide to avoid huge weights
+    np_test = np.asarray(test_images, dtype=np.float16).reshape(10000, 784) / 255
     bias = np.ones((60000, 1), dtype=np.float16)
     np_train = np.concatenate((np_train, bias), axis=1)  # concatenate matrix of 1s for bias
+    bias = np.ones((10000, 1), dtype=np.float16)
+    np_test = np.concatenate((np_test, bias), axis=1)
     weights = np.random.rand(10, 785) - .5
-    train_on_set(np_train, train_labels, weights, 60000, ETA, 1)
+    train_on_set(np_train, train_labels, np_test, test_labels, weights, 60000, ETA, 70)
 
 
 
