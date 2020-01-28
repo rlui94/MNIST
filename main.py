c714b288ebb5bf6cf2f84d2922b104085257181c
ryant <ryanlui.college@gmail.com>
2020-01-28 00:19:40 -0800

    Initial implementation of training func

--- main.py
+++ main.py
@@ -4,26 +4,33 @@ Code for extracting MNIST images based on https://medium.com/@mannasiladittya/co
 """
 
 from mnist.loader import MNIST
-import struct
 import random
+import datetime
 import numpy as np
 
 ETA = 0.1
+LOGFILE = 'log.txt'
+
+
+def train_on_set(train_images, train_labels, weights, size, learning_rate, epochs):
+    with open(LOGFILE, 'a') as file:
+        file.write("%s: \n" % (datetime.datetime.now()))
+    for n in range(0, size):
+        print(size)
+
 
 if __name__ == '__main__':
-    filename = {
-        'train_images': './images/train-images-idx3-ubyte',
-        'train_labels': './images/train-labels-idx1-ubyte',
-        'test_images': './images/t10k-images-idx3-ubyte',
-        'test_labels': './images/t10k-labels-idx1-ubyte'
-    }
-
     mndata = MNIST('./images/')
     train_images, train_labels = mndata.load_training()
     #test_images, test_labels = mndata.load_testing()
-    np_train = np.asarray(train_images, dtype=np.float32).reshape(60000, 28, 28) / 255
+    np_train = np.asarray(train_images, dtype=np.float32).reshape(60000, 28, 28) / 255  # divide to avoid huge weights
     bias = np.ones((60000, 28), dtype=np.float32)
-    weights = np.full((29,), ETA)
+    np_train = np.dstack((np_train, bias))  # concatenate matrix of 1s for bias
+    weights = np.full((29,), ETA)  # CHANGE THIS LATER
+    train_on_set(train_images, train_labels, weights, 5, ETA, 5)
+
+
+
 
 
 
